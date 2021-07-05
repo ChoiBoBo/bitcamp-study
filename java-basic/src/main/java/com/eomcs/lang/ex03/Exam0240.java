21c97fa524dbd4e37eec59787104674f9550e86a
ChoiBoBo <ohmyvia12@gmail.com>
2021-07-05 21:01:48 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/ex03/Exam0240.java
+++ java-basic/src/main/java/com/eomcs/lang/ex03/Exam0240.java
@@ -14,6 +14,9 @@ public class Exam0240 {
   public static void main(String[] args) {
     System.out.println(5 - 3);
     System.out.println(5 + (-3));
+
+    System.out.println();
+    System.out.println(1 + (-2));
   }
 }
 
