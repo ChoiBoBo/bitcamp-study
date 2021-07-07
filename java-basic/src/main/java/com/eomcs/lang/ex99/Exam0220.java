9a02a25bbfd88c878f9989c438664785f625c385
ChoiBoBo <ohmyvia12@gmail.com>
2021-07-07 23:19:31 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/ex99/Exam0220.java
+++ java-basic/src/main/java/com/eomcs/lang/ex99/Exam0220.java
@@ -7,6 +7,12 @@ public class Exam0220 {
     java.io.InputStream keyboard = System.in;
     java.util.Scanner keyScan = new java.util.Scanner(keyboard);
 
+    //보균
+    //new java.util.Scanner(keyboard); // 자바쪽에 유틸쪽에 스캐너를 꺼내자. // 물건을 제작
+    //제작할 때, 키보드 꼿어. 이걸 조정할 수 있는 keyScan(리모컨) 이용
+    // 준비된 리모컨으로 아래 이용
+
+
     // 팀 멤버의 정보를 입력 받아 출력하라.
     // 이름, 전화, 이메일, 나이, 재직여부
     // 예)
@@ -20,6 +26,7 @@ public class Exam0220 {
     // 20
     // 재직여부? y
     // y
+
     System.out.print("이름? ");
     System.out.println(keyScan.nextLine());
     System.out.print("전화? ");
