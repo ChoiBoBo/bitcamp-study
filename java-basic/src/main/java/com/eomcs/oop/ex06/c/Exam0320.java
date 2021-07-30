fa43c2607d60c92323def96d2969392684743b63
ChoiBoBo <ohmyvia12@gmail.com>
2021-07-30 12:38:59 +0900

    .

--- java-basic/src/main/java/com/eomcs/oop/ex06/c/Exam0320.java
+++ java-basic/src/main/java/com/eomcs/oop/ex06/c/Exam0320.java
@@ -16,8 +16,20 @@ public class Exam0320 {
     // => private 은 오버라이딩 자체가 불가능하기 때문에 접근 범위를 확대할 수 없다.
     //    @Override public m() {}
 
+
     @Override protected void m2() {} // default 보다 접근 범위를 확대했기 때문에 OK!
+
+
+    //    @Override private void m3() {} // protected 보다 접근 범위를 확대!
+    //    @Override public void m3() {} // protected 보다 접근 범위를 확대!
+    //    @Override protected void m3() {} // protected 보다 접근 범위를 확대!
     @Override public void m3() {} // protected 보다 접근 범위를 확대!
+
+    public void m4() {} // 접근 범위를 원래 대로 지정하는 것. OK!
+    public void m4() {} 
+    public void m4() {} 
+    public void m4() {} 
+
 
   }
 
