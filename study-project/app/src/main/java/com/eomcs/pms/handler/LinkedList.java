ba5cf860b999fee4da55fa2d5418e1c69e4a08cf
ChoiBoBo <ohmyvia12@gmail.com>
2021-08-07 22:33:41 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/handler/LinkedList.java
+++ study-project/app/src/main/java/com/eomcs/pms/handler/LinkedList.java
@@ -1,6 +1,6 @@
 package com.eomcs.pms.handler;
 
-public class LinkedList extends List {
+public class LinkedList implements List {
 
   static class Node {
     Object item;
@@ -28,7 +28,7 @@ public class LinkedList extends List {
       // 새로 만든 노드를 마지막 노드로 설정한다. 
       tail = node;
     }
-
+    //참고
     size++;
   }
 
