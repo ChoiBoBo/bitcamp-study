840b18d503b74c89eb5d294df35682e386e1f11c
ChoiBoBo <ohmyvia12@gmail.com>
2021-09-06 19:50:18 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -10,21 +10,9 @@ public class App {
 
   public static void main(String[] args) {
 
-    // 각 게시판의 게시글을 담을 메모리(boards 레퍼런스 배열과 size)를 준비한다.
-    // new BoardHandler()
-    //   - BoardHandler가 작업할 때 사용할 변수를 준비한다. 
-    //   - BoardHandler는 게시글을 다루는 작업을 한다.
     BoardHandler boardHandler = new BoardHandler();
-    BoardHandler boardHandler2 = new BoardHandler();
-    BoardHandler boardHandler3 = new BoardHandler();
-    BoardHandler boardHandler4 = new BoardHandler();
-    BoardHandler boardHandler5 = new BoardHandler();
-    BoardHandler boardHandler6 = new BoardHandler();
-
     MemberHandler memberHandler = new MemberHandler();
-
     ProjectHandler projectHandler = new ProjectHandler();
-
     TaskHandler taskHandler = new TaskHandler();
 
     while (true) {
@@ -39,11 +27,29 @@ public class App {
       } else if (input.equals("/member/list")) {
         memberHandler.list();
 
+      } else if (input.equals("/member/detail")) {
+        memberHandler.detail();
+
+      } else if (input.equals("/member/update")) {
+        memberHandler.update();
+
+      } else if (input.equals("/member/delete")) {
+        memberHandler.delete();
+
       }  else if (input.equals("/project/add")) {
         projectHandler.add(memberHandler);
 
       }  else if (input.equals("/project/list")) {
         projectHandler.list();
+
+      }  else if (input.equals("/project/detail")) {
+        projectHandler.detail();
+
+      }  else if (input.equals("/project/update")) {
+        projectHandler.update(memberHandler);
+
+      }  else if (input.equals("/project/delete")) {
+        projectHandler.delete();
 
       }  else if (input.equals("/task/add")) {
         taskHandler.add(memberHandler);
@@ -51,41 +57,29 @@ public class App {
       }  else if (input.equals("/task/list")) {
         taskHandler.list();
 
+      }  else if (input.equals("/task/detail")) {
+        taskHandler.detail();
+
+      }  else if (input.equals("/task/update")) {
+        taskHandler.update(memberHandler);
+
+      }  else if (input.equals("/task/delete")) {
+        taskHandler.delete();
+
       }  else if (input.equals("/board/add")) {
         boardHandler.add();
 
       }  else if (input.equals("/board/list")) {
         boardHandler.list();
 
-      }  else if (input.equals("/board2/add")) {
-        boardHandler2.add();
+      }  else if (input.equals("/board/detail")) {
+        boardHandler.detail();
 
-      }  else if (input.equals("/board2/list")) {
-        boardHandler2.list();
+      }  else if (input.equals("/board/update")) {
+        boardHandler.update();
 
-      }  else if (input.equals("/board3/add")) {
-        boardHandler3.add();
-
-      }  else if (input.equals("/board3/list")) {
-        boardHandler3.list();
-
-      }  else if (input.equals("/board4/add")) {
-        boardHandler4.add();
-
-      }  else if (input.equals("/board4/list")) {
-        boardHandler4.list();
-
-      }  else if (input.equals("/board5/add")) {
-        boardHandler5.add();
-
-      }  else if (input.equals("/board5/list")) {
-        boardHandler5.list();
-
-      }  else if (input.equals("/board6/add")) {
-        boardHandler6.add();
-
-      }  else if (input.equals("/board6/list")) {
-        boardHandler6.list();
+      }  else if (input.equals("/board/delete")) {
+        boardHandler.delete();
 
       } else {
         System.out.println("실행할 수 없는 명령입니다.");
