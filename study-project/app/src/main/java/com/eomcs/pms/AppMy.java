ec687605fee8baf88df4d662eb1c0358e771465c
ChoiBoBo <ohmyvia12@gmail.com>
2021-09-06 11:54:26 +0900

    .

--- study-project/app/src/main/java/com/eomcs/pms/AppMy.java
+++ study-project/app/src/main/java/com/eomcs/pms/AppMy.java
@@ -1,6 +1,5 @@
 package com.eomcs.pms;
 
-//app.java.03
 import com.eomcs.pms.handler.BoardHandlerMy;
 import com.eomcs.pms.handler.MemberHandler;
 import com.eomcs.pms.handler.ProjectHandler;
@@ -10,6 +9,14 @@ import com.eomcs.util.Prompt;
 public class AppMy {
 
   public static void main(String[] args) {
+
+
+    BoardHandlerMy boardHandlerMy = new BoardHandlerMy();
+    BoardHandlerMy boardHandlerMy2 = new BoardHandlerMy();
+    BoardHandlerMy boardHandlerMy3 = new BoardHandlerMy();
+    BoardHandlerMy boardHandlerMy4 = new BoardHandlerMy();
+    BoardHandlerMy boardHandlerMy5 = new BoardHandlerMy();
+    BoardHandlerMy boardHandlerMy6 = new BoardHandlerMy();
 
     while (true) {
       String input = Prompt.inputString("명령> ");
@@ -36,40 +43,40 @@ public class AppMy {
         TaskHandler.list();
 
       }  else if (input.equals("/board/add")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.add(boardHandlerMy);
 
       }  else if (input.equals("/board/list")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.list(boardHandlerMy);
 
       }  else if (input.equals("/board2/add")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.add(boardHandlerMy2);
 
       }  else if (input.equals("/board2/list")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.list(boardHandlerMy2);
 
       }  else if (input.equals("/board3/add")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.add(boardHandlerMy3);
 
       }  else if (input.equals("/board3/list")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.list(boardHandlerMy3);
 
       }  else if (input.equals("/board4/add")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.add(boardHandlerMy4);
 
       }  else if (input.equals("/board4/list")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.list(boardHandlerMy4);
 
       }  else if (input.equals("/board5/add")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.add(boardHandlerMy5);
 
       }  else if (input.equals("/board5/list")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.list(boardHandlerMy5);
 
       }  else if (input.equals("/board6/add")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.add(boardHandlerMy6);
 
       }  else if (input.equals("/board6/list")) {
-        BoardHandlerMy.add();
+        BoardHandlerMy.list(boardHandlerMy6);
 
       } else {
         System.out.println("실행할 수 없는 명령입니다.");
--- study-project/app/src/main/java/com/eomcs/pms/handler/BoardHandlerMy.java
+++ study-project/app/src/main/java/com/eomcs/pms/handler/BoardHandlerMy.java
@@ -1,44 +1,50 @@
 package com.eomcs.pms.handler;
 
-//java.01
 import java.sql.Date;
-import com.eomcs.pms.domain.BoardMy;
+import com.eomcs.pms.domain.Board;
 import com.eomcs.util.Prompt;
 
 public class BoardHandlerMy {
 
+  // 모든 게시판의 최대 배열 개수가 같기 때문에 다음 변수는 
+  // 그냥 static 필드로 남겨둔다.
   static final int MAX_LENGTH = 5;
 
-  static BoardMy[] boards = new BoardMy[MAX_LENGTH];
-  static int size = 0;
+  Board[] boards = new Board[MAX_LENGTH];
+  int size = 0;
 
-  public static void add() {
+  public static void add(BoardHandlerMy that) {
     System.out.println("[새 게시글]");
 
-    BoardMy board = new BoardMy();
+    Board board = new Board();
 
     board.no = Prompt.inputInt("번호? ");
     board.title = Prompt.inputString("제목? ");
     board.content = Prompt.inputString("내용? ");
     board.writer = Prompt.inputString("작성자? ");
     board.registeredDate = new Date(System.currentTimeMillis());
+    //    board.viewCount = 0; // 인스턴스 변수는 생성되는 순간 기본 값이 0으로 설정된다.
 
-    boards[size++] = board;
+    that.boards[that.size++] = board;
   }
 
-  public static void list() {
+  public static void list(BoardHandlerMy that) {
     System.out.println("[게시글 목록]");
-    for(int i = 0; i < size; i++) {
-      System.out.printf("%d, %s, %s, %s, %d\n",
-          boards[i].no,
-          boards[i].title,
-          boards[i].writer,
-          boards[i].registeredDate,
-          boards[i].viewCount,
-          boards[i].like);
-
-
+    for (int i = 0; i < that.size; i++) {
+      System.out.printf("%d, %s, %s, %s, %d, %d\n", 
+          that.boards[i].no, 
+          that.boards[i].title, 
+          that.boards[i].writer,
+          that.boards[i].registeredDate,
+          that.boards[i].viewCount, 
+          that.boards[i].like);
     }
   }
-
 }
+
+
+
+
+
+
+
